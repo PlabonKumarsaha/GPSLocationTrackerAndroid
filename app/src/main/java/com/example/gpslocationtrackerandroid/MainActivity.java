4c1fa03c6e2415d02f66c5cf3e5bb8722edfb1e2
PKS <pkumarsaha71@gmail.com>
2020-08-02 19:25:45 +0600

    lat longs speeds altitude were displayed in the UI

--- app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
+++ app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.gpslocationtrackerandroid;
 
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PERMISSION_FINE_REQUEST = 99;
     TextView tv_lat,tv_labellon,tv_lon,tv_altitude,tv_accuracy;
     TextView tv_speed,tv_sensor,tv_updates;
     Switch sw_locationsupdates,sw_gps;
@@ -56,6 +67,86 @@ public class MainActivity extends AppCompatActivity {
         //set priority about the service
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
 
+        sw_gps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(sw_gps.isChecked()){
+                    locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+                    tv_sensor.setText("Using GPS sensor...");
+                }else{
+                    locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+                    tv_sensor.setText("using WIFI + tower..");
+                }
+            }
+        });
+
+        //call the update GPS method
+
+        updateGPS();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSION_FINE_REQUEST :
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    updateGPS();
+                } else{
+                    Toast.makeText(getApplicationContext(),"Must need permission acess to use ",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    void updateGPS(){
+
+            //permit from the users to track GPS
+            //get the current location from the fused client
+            // update the UI to show the values in the screen
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                //user provided the permit
+                fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                     //we got permission.Now put the values of the location there.
+                    updateUiValues(location);
+
+                    }
+                });
+
+            }else{
+
+                //if permission is deined the again ask for permission .But now we have to look for mobile versions
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},PERMISSION_FINE_REQUEST);
+                }
+            }
+        }
+
+    private void updateUiValues(Location location) {
+        //update all the text view objects
+        tv_lat.setText(String.valueOf(location.getLatitude()));
+        tv_lon.setText(String.valueOf(location.getLongitude()));
+        tv_accuracy.setText(String.valueOf(location.getAccuracy()));
+
+        //check if the phone has altitude ..so check if we can get the altitude
+
+        if(location.hasAltitude()){
+            tv_altitude.setText(String.valueOf(location.getAltitude()));
+        } else{
+            tv_altitude.setText("Not availabe in your phone!");
+        }
+
+        if(location.hasSpeed()){
+            tv_speed.setText(String.valueOf(location.getSpeed()));
+        } else{
+            tv_speed.setText("Not availabe in your phone!");
+        }
 
 
 
