b2a448ecde97b08b551384eadb4b0194e5422c8a
PKS <pkumarsaha71@gmail.com>
2020-08-02 17:24:47 +0600

    UIs were introed

--- app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
+++ app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
@@ -9,10 +9,10 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_labellat,tv_lat,tv_labellon,tv_lon,tv_labelaltitude,tv_altitude,tv_labelaccuracy,tv_accuracy,tv_labelspeed;
-    TextView tv_speed,tv_labelsensor,tv_sensor,tv_labelupdates,tv_updates;
+    TextView tv_lat,tv_labellon,tv_lon,tv_altitude,tv_accuracy;
+    TextView tv_speed,tv_sensor,tv_updates;
     Switch sw_locationsupdates,sw_gps;
-    TextView tv_address,tv_lbladdress;
+    TextView tv_address;
     View divider;
 
 
@@ -20,6 +20,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_lat = findViewById(R.id.tv_lat);
+        tv_labellon = findViewById(R.id.tv_labellon);
+        tv_lon = findViewById(R.id.tv_lon);
+        tv_altitude = findViewById(R.id.tv_altitude);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_speed = findViewById(R.id.tv_speed);
+        tv_sensor = findViewById(R.id.tv_sensor);
+        tv_updates= findViewById(R.id.tv_updates);
+
+        sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
+        sw_gps = findViewById(R.id.sw_gps);
+
+        tv_address = findViewById(R.id.tv_address);
+
+        divider = findViewById(R.id.divider);
+
+
+
+
 
     }
 }
