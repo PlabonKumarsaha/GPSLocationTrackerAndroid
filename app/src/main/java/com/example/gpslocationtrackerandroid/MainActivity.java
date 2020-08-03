2e7500f6b69156c6ca4d9e42d59fa75c08c51f50
PKS <pkumarsaha71@gmail.com>
2020-08-03 19:50:01 +0600

    location click updated

--- app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
+++ app/src/main/java/com/example/gpslocationtrackerandroid/MainActivity.java
@@ -104,12 +104,10 @@ public class MainActivity extends AppCompatActivity {
 
                 if (sw_locationsupdates.isChecked()) {
                     //turn on tacking
-
                     startLocatonUpdates();
                 } else {
                     //turn off racking
                     stopLocationUpdates();
-
                 }
             }
         });
@@ -140,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         tv_updates.setText("locaton   tracked!");
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
-            
+
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
