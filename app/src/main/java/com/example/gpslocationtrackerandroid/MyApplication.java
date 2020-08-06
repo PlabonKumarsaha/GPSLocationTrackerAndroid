a9f368682c8027d182c5e381576f91fb449d0a7c
PKS <pkumarsaha71@gmail.com>
2020-08-07 05:55:55 +0600

    Save waypoints- MyApplication.java was created

--- app/src/main/java/com/example/gpslocationtrackerandroid/MyApplication.java
+++ app/src/main/java/com/example/gpslocationtrackerandroid/MyApplication.java
@@ -2,9 +2,22 @@ package com.example.gpslocationtrackerandroid;
 
 import android.app.Application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class MyApplication extends Application {
 
     private static MyApplication singleton;
+    List<Locale> myLocation;
+
+    public List<Locale> getMyLocation() {
+        return myLocation;
+    }
+
+    public void setMyLocation(List<Locale> myLocation) {
+        this.myLocation = myLocation;
+    }
 
     public static MyApplication getInstance() {
         return singleton;
@@ -13,5 +26,8 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        singleton = this;
+
+        myLocation = new ArrayList<>();
     }
 }
