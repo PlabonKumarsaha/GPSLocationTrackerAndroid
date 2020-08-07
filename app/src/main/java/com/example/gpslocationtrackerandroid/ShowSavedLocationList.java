59344ff35a8d87305908ce83a00435cdf3e7b2e2
PKS <pkumarsaha71@gmail.com>
2020-08-08 04:22:47 +0600

    ShowSavedLocationList activity was created

---
+++ app/src/main/java/com/example/gpslocationtrackerandroid/ShowSavedLocationList.java
@@ -0,0 +1,14 @@
+package com.example.gpslocationtrackerandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowSavedLocationList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_saved_location_list);
+    }
+}
